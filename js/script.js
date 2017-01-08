e6fd72b2dcdb9676ef4cd2d14982e640474bfb32
Jose Fremaint <jlbiglion@gmail.com>
2017-01-08 09:26:11 -0500

    Other Mayor Changes with day time
    header day time and other stuff..

--- js/script.js
+++ js/script.js
@@ -1,11 +1,34 @@
 $(document).ready(function(){
+
+	//time of the day header
+	      var d = new Date();
+        var hours = d.getHours();
+        if (hours > 17 || hours < 6){
+                // Night time - If time is after 5PM or before 6AM
+                $('header').addClass('night');
+								$('.goodevening').show();
+
+              }
+        else if (hours > 6 && hours < 12){
+                // Morning - After 6am and before 12pm
+								$('header').addClass('sunset');
+								$('.goodmorning').show();
+								$('.day-bg').show();
+							}
+        else{
+                // Afternoon- 12PM to 4PM
+                $('header').addClass('day');
+								$('.goodafternoon').show();
+							}
+
+
 	$('.nav-icon1').click(function(){
 		$(this).toggleClass('open');
     $('.nav').slideToggle();
 	});
-});
-
 $(".video-holder").click(function() {
   window.location = $(this).find("a").attr("href");
   return false;
+});
+
 });
