3d22a9ffcdd43d5dfd20c1a42b320766d13b67a5
Jose Fremaint <jlbiglion@gmail.com>
2017-01-09 06:04:35 -0500

    Morning hours changed
    Morning hour changed on the js

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ $(document).ready(function(){
 	//time of the day header
 	      var d = new Date();
         var hours = d.getHours();
-        if (hours > 17 || hours < 6){
+        if (hours > 17 || hours < 5){
                 // Night time - If time is after 5PM or before 6AM
                 $('header').addClass('night');
 								$('.goodevening').show();
               }
-        else if (hours > 6 && hours < 12){
+        else if (hours > 5 && hours < 12){
                 // Morning - After 6am and before 12pm
 								$('header').addClass('sunrise');
 								$('.goodmorning').show();
